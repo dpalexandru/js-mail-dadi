6e7434e7b1b61628f73308b1e89151c17efeb067
dpalexandru <alexpoian91@hotmail.com>
2025-07-04 15:44:02 +0200

    feat: print final risultat of game

--- dadi/js/script.js
+++ dadi/js/script.js
@@ -1,5 +1,18 @@
 // genero numeri casuali sia per il pc che per l'utente
 punteggioPc = Math.floor(Math.random() * 6) + 1;
-console.log (punteggioPc)
 punteggioGiocatore = Math.floor(Math.random() * 6) + 1;
-console.log (punteggioGiocatore)
+
+
+// Confronto i risultati e decido se ha vinto il pc, giocatore o è un pareggio
+
+if (punteggioPc === punteggioGiocatore) {
+  console.log(`Pc: ${punteggioPc} vs Giocatore: ${punteggioGiocatore}`)
+  console.log(`E' un pareggioo!!!!`)}
+  else if (punteggioPc > punteggioGiocatore) {
+  console.log(`Pc: ${punteggioPc} vs Giocatore: ${punteggioGiocatore}`)
+  console.log(`Ha vinto il Pc !!!! `)
+  }
+  else {
+  console.log(`Pc: ${punteggioPc} vs Giocatore: ${punteggioGiocatore}`)
+  console.log(`Ha vinto il Giocatoreee !!!!`)
+  }
