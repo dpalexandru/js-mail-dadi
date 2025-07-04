1f2020a45a184bec37cab2932088cb5b651230b1
dpalexandru <alexpoian91@hotmail.com>
2025-07-04 15:14:10 +0200

    feat: insert user email

---
+++ mail/js/script.js
@@ -0,0 +1,7 @@
+//Creo una lista mail degli invitati
+
+const invitati = ["ciccio@gmail.com", "pasticcio@gmail.com", "pippo@gmail.com","alex@gmail.com","boolean@gmail.com"];
+
+//chiedo la mail all'utente e la visualizzo
+const email = prompt("Inserisci la tua email:");
+console.log(`Hai inserito: ${email}`);
